05cb43feb7b420eea3df56d9827b6648c4ff3e53
Karla Cervantes <cervantes.karla14@gmail.com>
2020-11-10 19:51:51 -0600

    attempted tasks

--- index.js
+++ index.js
@@ -3,26 +3,37 @@ import { fifaData } from './fifa.js';
 // ⚽️ M  V P ⚽️ //
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 1: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
-Investigate the data above. Practice accessing data by console.log-ing the following pieces of data note, you may want to filter the data first 😉*/
-
+Investigate the data above. Practice accessing data by console.log-ing the 
+following pieces of data note, you may want to filter the data first 😉*/
+const finals = fifaData.filter(function(item){
+    return item.Year === 2014 && item.Stage === 'Final';
+});
+console.log(finals);
 //(a) Home Team name for 2014 world cup final
-
+console.log('task 1 a:', finals[0]['Home Team Name']);
 //(b) Away Team name for 2014 world cup final
-
+console.log('task 1 a:', finals[0]['Away Team Name']);
 //(c) Home Team goals for 2014 world cup final
-
+console.log('task 1 a:', finals[0]['Home Team Goals']);
 //(d) Away Team goals for 2014 world cup final
-
+console.log('task 1 a:', finals[0]['Away Team Goals']);
 //(e) Winner of 2014 world cup final */
-
+console.log('task 1 a:', finals[0]['Win conditions']);
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
-Create a function called  getFinals that takes `data` as an argument and returns an array of objects with only finals data */
+Create a function called  getFinals that takes `data` as an argument and 
+returns an array of objects with only finals data */
 
-function getFinals(/* code here */) {
-   /* code here */
+function getFinals(arr) {
+   let newar = [];
+   for(let i = 0; i < arr.length; i++){
+       if(arr[i].Stage === 'Final'){
+           newar.push(arr[i])
+       }
+   }
+   return newar;
 }
-
+console.log(getFinals(fifaData));
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
